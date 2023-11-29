c512549f14c0aaae334075d31a641582808a0e65
Jesse <ebennyems@gmail.com>
2023-11-28 21:01:53 -0800

    work

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for num in range(0, 99):
-    print("{num:d} = {hex_n}".format(
-        hex_n=hex(number)), end="\n")
+    print("{num_A:d} = {hex_n}".format(
+        hex_n=hex(num), num_A=num), end="\n")
