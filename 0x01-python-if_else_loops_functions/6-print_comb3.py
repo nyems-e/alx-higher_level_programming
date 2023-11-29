aab0f037ac7bd5364f25b8cff52ba94af03ff939
Jesse <ebennyems@gmail.com>
2023-11-29 14:28:52 -0800

    !

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(10):
+    for x in range(10):
+        if i == 8 and x == 9:
+            print("{:d}{:d}".format(i, x),end = "\n")
+        elif i != x and i < x:
+            print("{:d}{:d}".format(i, x), end=",")
+
+
+
