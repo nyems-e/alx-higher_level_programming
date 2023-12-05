42122b00a5f64d84c01a2ae0b0f7e97dc13b65fd
Jesse <ebennyems@gmail.com>
2023-12-05 14:36:18 -0800

    !

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_s = ''
-     for i in my_string:
-        if i == 'c' or i == 'C':
-            continue
-        new_s += i
-     return (new_s)
+    if my_string:
+        for i in my_string:
+            if i == 'c' or i == 'C':
+                continue
+            new_s += i
+    return (new_s)
