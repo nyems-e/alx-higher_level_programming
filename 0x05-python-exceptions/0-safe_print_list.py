6cd8991929af86002f98c56efe21f3d624899730
Jesse <ebennyems@gmail.com>
2023-12-18 15:17:27 -0800

    !

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     track = 0
     while x > 0:
         try:
-            print("{}".format(my_list[track]), end = "")
+            print("{}".format(my_list[track]), end="")
             track += 1
             x -= 1
         except (IndexError):
